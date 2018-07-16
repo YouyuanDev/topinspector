adf54acb5b94a1222bcc12299378417418490128
ketewang <ketewang@youyuantech.com>
2018-07-16 19:27:03 +0800

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,247 @@
+"use strict";
+
+//Rage Template
+//Designerd by: #
+jQuery(document).ready(function ($) {
+
+//for Preloader
+
+    $(window).load(function () {
+        $("#loading").fadeOut(500);
+    });
+
+
+    /*---------------------------------------------*
+     * Mobile menu
+     ---------------------------------------------*/
+    $('#navbar-menu').find('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 80)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+
+
+
+    /*---------------------------------------------*
+     * WOW
+     ---------------------------------------------*/
+
+    var wow = new WOW({
+        mobile: false // trigger animations on mobile devices (default is true)
+    });
+    wow.init();
+
+// magnificPopup
+
+    $('.popup-img').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    $('.video-link').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        iframe: {
+  markup: '<div class="mfp-iframe-scaler">'+
+            '<div class="mfp-close"></div>'+
+            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+  patterns: {
+    youtube: {
+      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+      id: 'v=', // String that splits URL in a two parts, second part should be %id%
+      // Or null - full URL will be returned
+      // Or a function that should return %id%, for example:
+      // id: function(url) { return 'parsed id'; }
+
+      src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+    }
+    // you may add here more sources
+
+  },
+
+  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+}
+ 
+    });
+
+
+
+// slick slider active Home Page Tow
+    $(".testimonial_slid").slick({
+        dots: false,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: "<i class='fa fa-angle-left nextprevleft'></i>",
+        nextArrow: "<i class='fa fa-angle-right nextprevright'></i>",
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+
+
+//    featured slider
+    $('.featured_slider').slick({
+        centerMode: true,
+        dote: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        speed: 1500,
+        index: 2,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+
+
+//---------------------------------------------
+// Counter 
+//---------------------------------------------
+
+    $('.statistic-counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+//---------------------------------------------
+// Scroll Up 
+//---------------------------------------------
+    
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+    });
+    $('.scrollup').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+        return false;
+    });
+
+
+
+
+
+//About us accordion 
+
+    $("#faq_main_content").collapse({
+        accordion: true,
+        open: function () {
+            this.addClass("open");
+            this.css({height: this.children().outerHeight()});
+        },
+        close: function () {
+            this.css({height: "0px"});
+            this.removeClass("open");
+        }
+    });
+
+
+
+
+
+//Team Skillbar active js
+
+    jQuery('.teamskillbar').each(function () {
+        jQuery(this).find('.teamskillbar-bar').animate({
+            width: jQuery(this).attr('data-percent')
+        }, 6000);
+    });
+   //点击显示微信公众号图片 
+   openWeixinQRCode();
+
+    //加载地图
+     //loadCompanyLocation();
+
+   //加载地图结束
+
+    //End
+
+});
+/*新增函数*/
+function openWeixinQRCode(){
+	$('#chatting_our').click(function(){
+		$('.weixin_qrcode img').show();
+	});
+}
+function loadCompanyLocation(){
+	    var sContent =" <br>地址:";
+        var map = new BMap.Map("map_canvas");
+        var point = new BMap.Point(121.497982,31.384589);  //可以通过百度地图坐标拾取系统 http://api.map.baidu.com/lbsapi/getpoint/index.html
+        var marker = new BMap.Marker(point);
+        var infoWindow = new BMap.InfoWindow(sContent);
+        map.centerAndZoom(point, 15);
+        map.addOverlay(marker);
+        marker.openInfoWindow(infoWindow);
+        infoWindow.redraw();
+}
+
+function openFunctionAndService(){
+	//alert(1);
+	$('.sec-animate0').addClass('animated zoomInLeft');
+	$('.sec-animate1').addClass('animated zoomInUp');
+	$('.sec-animate2').addClass('animated zoomInRight');
+	setTimeout(function(){
+        $('.sec-animate0').removeClass('zoomInLeft');
+	   $('.sec-animate1').removeClass('zoomInUp');
+	   $('.sec-animate2').removeClass('zoomInRight');
+    }, 1000);
+}
+function openCompanyBlog(){
+	$('.sec-animate3').addClass('animated slideInLeft');
+	$('.sec-animate4').addClass('animated slideInDown');
+	$('.sec-animate5').addClass('animated slideInRight');
+	setTimeout(function(){
+	     $('.sec-animate3').removeClass('slideInLeft');
+	   $('.sec-animate4').removeClass('slideInDown');
+	   $('.sec-animate5').removeClass('slideInRight');
+    }, 1000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
